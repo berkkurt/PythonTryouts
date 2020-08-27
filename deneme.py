c594e58059fd48864f6f02630bc111b495f54f25
berkkurt <berkk1994@hotmail.com>
2020-08-27 23:32:49 +0300

    yenideger

--- deneme.py
+++ deneme.py
@@ -1,11 +1,29 @@
 print("bu bir denemedir")
 
+
 def berk():
     pass
 
+
+num1 = 10
+
+
 def toplama(num1, num2):
+    num1 = 1
     return num1 + num2
 
-print(toplama(2,3))
 
-print()
+def çıkarma(num1, num2):
+    pass
+
+
+berk()
+print(toplama(2, 3))
+for x in range(100, -1):
+    if x == 80:
+        continue
+    print(x)
+
+
+def rrr():
+    pass
